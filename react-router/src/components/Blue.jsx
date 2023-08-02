72872bdc7d164a7bc68d9e72f4a00a2f7e952d9b
prestige146 <rudy.gabriel1@aol.com>
2023-08-02 00:26:35 -0400

    navbar

---
+++ react-router/src/components/Blue.jsx
@@ -0,0 +1,11 @@
+const Blue = () => {
+    return(
+ <>
+      <div className="Blue">
+        <h1>Blue</h1>
+      </div>
+  </>
+    );
+    }
+    
+   export default Blue;
---
+++ react-router/src/components/Red.jsx
@@ -0,0 +1,11 @@
+const Red = () => {
+    return(
+ <>
+      <div className="Red">
+        <h1>Red</h1>
+      </div>
+  </>
+    );
+    }
+    
+   export default Red;
